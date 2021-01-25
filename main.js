5ec3bdbc566a8236b70dd5c3c250bf576eabfdc5
beahoop <beahoopdesign@gmail.com>
2021-01-25 15:59:25 -0500

    updating style sheet

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("hey");
+
+const image1 = document.querySelector('.image1');
+function changeImage1() {
+  // if (image1.src = "./images/Puppy1.jpg") {
+    image1.src = "./images/Puppy3.jpg"
+  // } else if(image1.src = "./images/Puppy1.jpg") {
+  //   image1.src = "./images/Puppy1.jpg";
+  // }
+};
+image1.addEventListener('click', changeImage1);
+
+const imageBtn = document.querySelector('.imagebtn');
+const image2 = document.querySelector('.image2');
+function changeImage2() {
+  image2.src = "./images/dog1.gif";
+}
+
+imageBtn.addEventListener('click', changeImage2);
+
+const image3 = document.querySelector('.image3');
+function changeImage3() {
+  image3.src = "./images/Puppy1.jpg";
+}
+image3.addEventListener('click', changeImage3);
