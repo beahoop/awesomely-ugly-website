77f5ad45590ee0db1cb8257800b84e6c60ebcb2a
Sarah Bea Hooper <70774362+beahoop@users.noreply.github.com>
2021-01-26 09:03:28 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ console.log("hey");
 
 const image1 = document.querySelector('.image1');
 function changeImage1() {
-    image1.src = "./images/Puppy3.jgp"
-
+    image1.src = "./images/Puppy3.jpg"
 };
 image1.addEventListener('click', changeImage1);
 
@@ -17,6 +16,6 @@ imageBtn.addEventListener('click', changeImage2);
 
 const image3 = document.querySelector('.image3');
 function changeImage3() {
-  image3.src = "./images/Puppy1.JGP";
+  image3.src = "./images/Puppy1.JPG";
 }
 image3.addEventListener('click', changeImage3);
