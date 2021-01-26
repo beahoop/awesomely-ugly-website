c97f5b9fa193edd5bb626ac0ac48dc595c0a1100
Sarah Bea Hooper <70774362+beahoop@users.noreply.github.com>
2021-01-26 09:04:16 -0500

    fixed images

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-console.log("hey");
-
-const image1 = document.querySelector('.image1');
-function changeImage1() {
-    image1.src = "./images/Puppy3.jpg"
-};
+// console.log("hey");
+//
+// const image1 = document.querySelector('.image1');
+// function changeImage1() {
+//     image1.src = "./images/Puppy3.jpg"
+// };
 image1.addEventListener('click', changeImage1);
 
 const imageBtn = document.querySelector('.imagebtn');
@@ -12,10 +12,10 @@ function changeImage2() {
   image2.src = "./images/dog1.gif";
 }
 
-imageBtn.addEventListener('click', changeImage2);
-
-const image3 = document.querySelector('.image3');
-function changeImage3() {
-  image3.src = "./images/Puppy1.JPG";
-}
-image3.addEventListener('click', changeImage3);
+// imageBtn.addEventListener('click', changeImage2);
+//
+// const image3 = document.querySelector('.image3');
+// function changeImage3() {
+//   image3.src = "./images/Puppy1.JPG";
+// }
+// image3.addEventListener('click', changeImage3);
