4d55d24ce773a7ba20425137d7f6b1b55390539f
Sarah Bea Hooper <70774362+beahoop@users.noreply.github.com>
2021-01-26 08:05:50 -0500

    updated file names

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("hey");
 const image1 = document.querySelector('.image1');
 function changeImage1() {
   // if (image1.src = "./images/Puppy1.jpg") {
-    image1.src = "./images/Puppy3.jpg"
+    image1.src = "./images/Puppy3.JGP"
   // } else if(image1.src = "./images/Puppy1.jpg") {
   //   image1.src = "./images/Puppy1.jpg";
   // }
@@ -20,6 +20,6 @@ imageBtn.addEventListener('click', changeImage2);
 
 const image3 = document.querySelector('.image3');
 function changeImage3() {
-  image3.src = "./images/Puppy1.jpg";
+  image3.src = "./images/Puppy1.JGP";
 }
 image3.addEventListener('click', changeImage3);
